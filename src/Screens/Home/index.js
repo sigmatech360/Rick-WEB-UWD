b3ef06df252d8324523159e8625de5697bb71633
sigmatech360 <syed.danial@sigmatechservices.net>
2024-11-21 15:52:34 -0800

    add

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,36 +1,33 @@
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-  import VolunteerModal from '../../components/modal';
-  import React,{useState} from "react";
+import VolunteerModal from "../../components/modal";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 
-import announcementunderline from '../../Assets/images/announcementunderline.svg'
+import announcementunderline from "../../Assets/images/announcementunderline.svg";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
- 
-
 import donateicon from "../../Assets/images/donateicon.png";
-import availabletagline from '../../Assets/images/availabletagline.png'
-import apartunderline from '../../Assets/images/apartunderline.png'
+import availabletagline from "../../Assets/images/availabletagline.png";
+import apartunderline from "../../Assets/images/apartunderline.png";
 import "./index.css";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
 import "swiper/css";
 import tagline from "../../Assets/images/tagline.png";
 
-import programunderline from '../../Assets/images/programunderline.svg'
+import programunderline from "../../Assets/images/programunderline.svg";
 import announcement1 from "../../Assets/images/announcement1.png";
 import announcement2 from "../../Assets/images/announcement2.png";
- 
-import changeworld from '../../Assets/images/changeworld.png'
 
-import wedotagline from '../../Assets/images/wedotagline.png'
+import changeworld from "../../Assets/images/changeworld.png";
 
-  
+import wedotagline from "../../Assets/images/wedotagline.png";
+
 import housingcard1 from "../../Assets/images/housingcard1.png";
 import Layout from "../../components/layout";
 import Contact from "../../components/contact";
@@ -38,22 +35,26 @@ import Sponsor from "../../components/sponsor";
 import Ourpodcasts from "../../components/ourpodcast";
 import Events from "../../components/events";
 function Home() {
- 
   const [showModal, setShowModal] = useState(false);
 
   const handleShow = () => setShowModal(true);
   const handleClose = () => setShowModal(false);
-
-
+  useEffect(() => {
+    handleShow();
+  }, []);
 
   const PrevArrow = (props) => {
-
-
-    
     const { onClick } = props;
     return (
-      <button type="button" className="slick-arrow slick-prev mb-2 " onClick={onClick}>
-        <span className="arrow-icon">  <i class="fa fa-arrow-left" aria-hidden="true"></i></span>
+      <button
+        type="button"
+        className="slick-arrow slick-prev mb-2 "
+        onClick={onClick}
+      >
+        <span className="arrow-icon">
+          {" "}
+          <i class="fa fa-arrow-left" aria-hidden="true"></i>
+        </span>
       </button>
     );
   };
@@ -61,8 +62,15 @@ function Home() {
   const NextArrow = (props) => {
     const { onClick } = props;
     return (
-      <button type="button" className="slick-arrow slick-next mb-2" onClick={onClick}>
-        <span className="arrow-icon">  <i class="fa fa-arrow-right" aria-hidden="true"></i></span>
+      <button
+        type="button"
+        className="slick-arrow slick-next mb-2"
+        onClick={onClick}
+      >
+        <span className="arrow-icon">
+          {" "}
+          <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        </span>
       </button>
     );
   };
@@ -112,7 +120,6 @@ function Home() {
 
   return (
     <>
-
       <Layout>
         <section className="homeless-intervention">
           <Swiper
@@ -146,8 +153,8 @@ function Home() {
                       </h1>
 
                       <p className="mb-4">
-                        Our shelters and supportive service programs assist people
-                        who are homeless or at risk of being homeless.
+                        Our shelters and supportive service programs assist
+                        people who are homeless or at risk of being homeless.
                       </p>
                       <div className=" ">
                         {/* <button className="btn btn-warning text-dark me-3">
@@ -180,8 +187,8 @@ function Home() {
                         <span className="text-warning">Homelessness</span>
                       </h1>
                       <p className="mb-4">
-                        Our shelters and supportive service programs assist people
-                        who are homeless or at risk of being homeless.
+                        Our shelters and supportive service programs assist
+                        people who are homeless or at risk of being homeless.
                       </p>
                       <div className="d-flex">
                         <button className="btn btn-warning text-dark me-3">
@@ -210,8 +217,8 @@ function Home() {
                         <span className="text-warning">Homelessness</span>
                       </h1>
                       <p className="mb-4">
-                        Our shelters and supportive service programs assist people
-                        who are homeless or at risk of being homeless.
+                        Our shelters and supportive service programs assist
+                        people who are homeless or at risk of being homeless.
                       </p>
                       <div className="d-flex">
                         <button className="btn btn-warning text-dark me-3">
@@ -229,14 +236,12 @@ function Home() {
           </Swiper>
         </section>
 
-
         <div>
-      <Button variant="primary" onClick={handleShow}>
-        Open Modal
-      </Button>
-      <VolunteerModal show={showModal} handleClose={handleClose} />
-    </div>
-
+          <Button variant="primary" onClick={handleShow}>
+            Open Modal
+          </Button>
+          <VolunteerModal show={showModal} handleClose={handleClose} />
+        </div>
 
         <section className="homeless-intervention ">
           <div className="container-fluid">
@@ -245,7 +250,10 @@ function Home() {
               <div className="lft col-lg-8 py-5 col-md-12   d-flex flex-column justify-content-center ">
                 <div className=" content   left-section">
                   <h2 className="how-we-do   mb-3">
-                    How <span className="highlight-text">We Do <img src={wedotagline} className="wedotagline" /> </span>
+                    How{" "}
+                    <span className="highlight-text">
+                      We Do <img src={wedotagline} className="wedotagline" />{" "}
+                    </span>
                     It
                   </h2>
                   <p className="wedopara">
@@ -256,8 +264,8 @@ function Home() {
                     community’s most vulnerable population. HIS-OC does not and
                     shall not discriminate on the basis of race, color, religion
                     (creed), gender, gender expression, age, national origin
-                    (ancestry), disability, marital status, sexual orientation, or
-                    military status, in any of its activities or operations.
+                    (ancestry), disability, marital status, sexual orientation,
+                    or military status, in any of its activities or operations.
                   </p>
                 </div>
               </div>
@@ -266,8 +274,12 @@ function Home() {
               <div className="col-lg-4 col-md-12 right-section d-flex flex-column justify-content-center p-5 text-center text-md-start">
                 <h2 className="right-section-text">
                   You Must Be The <br />
-                  <span className="text-warning">Change</span> You Wish To <br />
-                  See In The <span className="text-success position-relative">World <img className="changeworld" src={changeworld} /></span>
+                  <span className="text-warning">Change</span> You Wish To{" "}
+                  <br />
+                  See In The{" "}
+                  <span className="text-success position-relative">
+                    World <img className="changeworld" src={changeworld} />
+                  </span>
                 </h2>
               </div>
             </div>
@@ -277,11 +289,14 @@ function Home() {
         <section className="what-sets-us-apart text-white py-5">
           <div className="container text-center">
             <h2 className="setus  fw-bold mb-3">
-              What Sets Us <span className="text-warning  position-relative">Apart  <img className="apartunderline" src={apartunderline} /></span>
+              What Sets Us{" "}
+              <span className="text-warning  position-relative">
+                Apart <img className="apartunderline" src={apartunderline} />
+              </span>
             </h2>
             <p className="mb-0">
-              We are different from an emergency shelter or single program shelter
-              because we provide a longer-term guided process to achieve
+              We are different from an emergency shelter or single program
+              shelter because we provide a longer-term guided process to achieve
               self-sufficiency. Our programs require a commitment on the part of
               our clients and provide tools so that a return to homelessness is
               prevented. Homeless Intervention Services of Orange County
@@ -295,7 +310,11 @@ function Home() {
         <section className="help-available py-5">
           <div className="container text-center mb-4">
             <h2 className="helptagline fw-bold mb-3">
-              Help is <span className="text-success position-relative">Available    <img className="availabletagline" src={availabletagline} /></span>
+              Help is{" "}
+              <span className="text-success position-relative">
+                Available{" "}
+                <img className="availabletagline" src={availabletagline} />
+              </span>
             </h2>
             <p className=" mb-4">
               We've been helping people find their way home since 1989.
@@ -332,13 +351,19 @@ function Home() {
                   </p>
                   <h2 className="fw-bold text-white  px-4 ">
                     Transitional Housing{" "}
-                    <span className="text-warning position-relative">Program <img className="programunderline" src={programunderline} /></span>
+                    <span className="text-warning position-relative">
+                      Program{" "}
+                      <img
+                        className="programunderline"
+                        src={programunderline}
+                      />
+                    </span>
                   </h2>
                   <p className="para px-4 ">
                     We have 54 beds in 3 shelters that provide a safe place to
-                    stay for families. Residents can stay for 4 to 6 months, in a
-                    home-like environment, while they establish themselves in a
-                    career and build up savings in order to transition into
+                    stay for families. Residents can stay for 4 to 6 months, in
+                    a home-like environment, while they establish themselves in
+                    a career and build up savings in order to transition into
                     permanent housing. We house over 150 men, women and children
                     each year in a spirit of dignity and love through our three
                     residences.
@@ -357,15 +382,19 @@ function Home() {
                       alt="Youth Shelter 1"
                       className="img-fluid rounded mb-2"
                     />
-                    <h5 className="cardtitle fw-bold">Transitional Aged  <span className="youthshelter">Youth Shelter </span></h5>
+                    <h5 className="cardtitle fw-bold">
+                      Transitional Aged{" "}
+                      <span className="youthshelter">Youth Shelter </span>
+                    </h5>
                     <p className="small mb-2">
                       Combining Housing Education and Skills for Students. A
                       transitional 12-24 month program for 18-24-year-olds.
                       Residents attend college, trade school, or complete a high
-                      school diploma/GED requirements. Many of our residents were
-                      in the foster care system and do not have help navigating
-                      the education system. Our case managers help them set
-                      personal goals and connect them to appropriate resources..
+                      school diploma/GED requirements. Many of our residents
+                      were in the foster care system and do not have help
+                      navigating the education system. Our case managers help
+                      them set personal goals and connect them to appropriate
+                      resources..
                     </p>
                     <button className="cardbtn btn btn-outline-primary btn-sm">
                       Learn More
@@ -393,14 +422,9 @@ function Home() {
           </div>
         </section>
 
+        <Events />
 
- 
-
-
-<Events/>
-
-<Ourpodcasts/>
-
+        <Ourpodcasts />
 
         {/* Announcement section */}
 
@@ -411,31 +435,50 @@ function Home() {
               <div className="col-md-12 col-lg-5 newsleftsection d-flex flex-column justify-content-center">
                 <div className="text-content">
                   <h3 className="title fw-bold mb-4  ">
-                    News & <span className="text-warning position-relative">Announcements
-                      <img className="announcementunderline" src={announcementunderline} alt="Underline" />
+                    News &{" "}
+                    <span className="text-warning position-relative">
+                      Announcements
+                      <img
+                        className="announcementunderline"
+                        src={announcementunderline}
+                        alt="Underline"
+                      />
                     </span>
                   </h3>
                   <p className="para ">
-                    Last year we helped over 1000 people with our programs and guided 85% of those in our resident programs to permanent housing. Since 1989, we have guided over 5000 people out of homelessness and back into productive and fulfilling lives.
+                    Last year we helped over 1000 people with our programs and
+                    guided 85% of those in our resident programs to permanent
+                    housing. Since 1989, we have guided over 5000 people out of
+                    homelessness and back into productive and fulfilling lives.
                   </p>
-                  <button className="allannouncementbtn btn btn-outline-light mt-3">View all Announcements</button>
+                  <button className="allannouncementbtn btn btn-outline-light mt-3">
+                    View all Announcements
+                  </button>
                 </div>
               </div>
 
               {/* Right Carousel Section */}
-              <div className="col-lg-6
-           col-md-12 position-relative">
+              <div
+                className="col-lg-6
+           col-md-12 position-relative"
+              >
                 <div className="mt-5">
                   <Slider {...settings}>
                     {newsData.map((news) => (
                       <div key={news.id} className="p-4 mt-4">
                         <div className="news-card bg-white shadow-sm rounded">
-                          <img src={news.image} alt={news.title} className="img-fluid rounded mb-2" />
+                          <img
+                            src={news.image}
+                            alt={news.title}
+                            className="img-fluid rounded mb-2"
+                          />
                           <div className="p-3">
                             <p className="text-muted small mb-1">{news.date}</p>
                             <h5 className="fw-bold mb-2">{news.title}</h5>
                             <p className="small mb-3">{news.description}</p>
-                            <button className="btn btn-outline-primary btn-sm">Learn More</button>
+                            <button className="btn btn-outline-primary btn-sm">
+                              Learn More
+                            </button>
                           </div>
                         </div>
                       </div>
@@ -451,13 +494,8 @@ function Home() {
 
         <Contact />
 
-
         <Sponsor />
-
-
-
       </Layout>
-
     </>
   );
 }
